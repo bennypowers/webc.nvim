63835d5c7ab86e5fc28825b95f612fa5debbf467
Benny Powers <web@bennypowers.com>
2023-04-25 22:29:13 +0300

    fix(syntax): highlight for `webc:root` value

--- queries/html/highlights.scm
+++ queries/html/highlights.scm
@@ -2,6 +2,12 @@
 ((attribute_name) @keyword
                   (#offset! @keyword 0 5 0 0)
                   (#match? @keyword "^webc:"))
-; ((attribute_name) @namespace
-;                   (#offset! @keyword 0 0 0 -4)
-;                   (#match? @keyword "^webc:"))
+
+((attribute_name) @_name
+                  [
+                   (attribute_value) @constant
+                   (quoted_attribute_value
+                     (attribute_value) @constant)
+                   ]
+                  (#is-filetype? "webc")
+                  (#eq? @_name "webc:root"))
