c172cb40b9f2ae9dcf98057ce2403fecba7b1710
Benny Powers <web@bennypowers.com>
2025-08-03 13:52:49 +0300

    fix: when predicate doesn't operate on a buffer

--- lua/webc.lua
+++ lua/webc.lua
@@ -2,7 +2,11 @@ local M = {}
 
 function M.setup()
   vim.treesitter.query.add_predicate('is-filetype?', function(_, _, bufnr, pred)
-    return vim.bo[bufnr].filetype == pred[2]
+    if type(bufnr) == "number" then
+      return vim.bo[bufnr].filetype == pred[2]
+    else
+      return false
+    end
   end)
   vim.filetype.add { extension = { webc = 'webc' } }
   vim.treesitter.language.add('html', { filetype = 'webc', })
